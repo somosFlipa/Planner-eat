2b0b3fca26ddd1d4e8765e01222c9d6431e747c8
lunabartolozzi <98171421+lunaflipa@users.noreply.github.com>
2022-03-08 12:36:53 -0300

    subida

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+
+
+app.use(express.json());
+
+app.use(
+  express.urlencoded({
+    extended: true,}));
+
+app.listen(PORT, () => {
+  console.log(`Server start on ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.json({ user: 'geek'})
+});
